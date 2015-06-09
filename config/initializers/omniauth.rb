e554cf7fb54fb4f376f9be370cb4dab4309de662
Darin London <darin.london@duke.edu>
2015-06-09 15:59:49 -0400

    removed unnecessary attributes from copypasta

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,10 +3,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     :uid_field                 => "uid",
     :name_field                => "displayName",
     :info_fields => {
-      :email    => "mail",
-      :location => "contactAddress",
-      :image    => "photo_url",
-      :phone    => "contactPhone"
+      :mail    => "mail"
     },
     :debug => true,
     :request_type => :header
