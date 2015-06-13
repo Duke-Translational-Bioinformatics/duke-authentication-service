3f51227025ba2152a17fe67a1761f1fb07d7b1b5
Darin London <darin.london@duke.edu>
2015-06-13 10:52:51 -0400

    Consumer passes the new specification requiring it to provide a signed_token method which returns an encoded JWT signed with the consumer secret

--- app/models/consumer.rb
+++ app/models/consumer.rb
@@ -1,4 +1,8 @@
 class Consumer < ActiveRecord::Base
   validates :uuid, presence: true, uniqueness: true
   validates :secret, presence: true
+
+  def signed_token(token)
+    JWT.encode(token, secret)
+  end
 end
