75b72b6e59b079216302877c6b8e4f31298c8774
Darin London <darin.london@duke.edu>
2015-06-15 16:43:38 -0400

    implemented that endpoints required for a complete OAUTH2 authentication
    authentication_controller implements
      authenticate: checks the params for required client_id, state, checks whether the Consumer exists for the client_id
        responds with 401 invalid_response if these are not satisfied
        sets session variables and redirects_to shibboleth_login_url if these are satisfied
    
      handle_shibboleth:  parses the shibboleth provided attributes into the session
        If the user has already allowed the consumer to access their profile, creates a new user token and access_token and
          redirects back to the consumer with the expected fragment
        If the user has not already allowed the consumer to access their profile, redirects_to authorize_url
    
    authorize: presents a form to the user to allow them to authorize the consumer access to their profile
    
    process_authorization: determines whether user allowed or denied the consumer access to their profile
      if the user allows, creates a new User object, creates a new user token and access_token and
          redirects back to the consumer with the expected fragment
      if the user denies, redirects to the consumer with a fragment with an error and the original state
    
    authorization_controller_spec passes

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,76 @@
+class AuthenticationController < ApplicationController
+  def authenticate
+    [:client_id, :state].each do |required_param|
+      unless params.has_key?(required_param)
+        render plain: 'invalid_request', status: 401, layout: false
+        return
+      end
+    end
+    @consumer = Consumer.where(uuid: params[:client_id]).first
+    if @consumer
+      session[:client_id] = params[:client_id]
+      session[:state] = params[:state]
+      if session[:scope]
+        session[:scope] = params[:scope]
+      end
+      session[:respose_type] = 'token'
+      redirect_to shibboleth_login_url
+    else
+      render plain: 'invalid_request', status: 401, layout: false
+      return
+    end
+  end
+
+  def handle_shibboleth
+    session[:uid] = request.env['omniauth.auth'][:uid]
+    session[:display_name] = request.env['omniauth.auth'][:info][:name]
+    session[:mail] = request.env['omniauth.auth'][:info][:mail]
+    @user = User.where(uid: session[:uid]).first
+    if @user
+      redirect_to_consumer
+    else
+      redirect_to authorize_url
+    end
+  end
+
+  def authorize
+    @user = User.new(uid: session[:uid])
+  end
+
+  def process_authorization
+    if params[:submitted] == 'allow'
+      @user = User.create(uid: session[:uid])
+    end
+    redirect_to_consumer
+  end
+
+  private
+
+  def redirect_to_consumer
+    consumer = Consumer.where(uuid: session[:client_id]).first
+    if @user
+      @token = @user.token(
+        display_name: session[:display_name],
+        mail: session[:mail],
+        scope: session[:scope] || Rails.application.config.default_scope
+      )
+      access_token = consumer.signed_token({
+        access_token: @token
+      })
+      token_ttl = $redis.ttl(@token)
+      params = {
+        access_token: access_token,
+        token_type: 'Bearer',
+        state: session[:state],
+        expires_in: token_ttl,
+        scope: session[:scope] || Rails.application.config.default_scope
+      }
+    else
+      params = {
+        error: 'access_denied',
+        state: session[:state]
+      }
+    end
+    redirect_to(consumer.redirect_uri+'#'+params.to_query)
+  end
+end
---
+++ spec/controllers/authentication_controller_spec.rb
@@ -0,0 +1,163 @@
+require 'rails_helper'
+require 'securerandom'
+
+RSpec.describe AuthenticationController, type: :controller do
+  let(:user) { FactoryGirl.create(:user) }
+  let(:first_time_user) { FactoryGirl.build(:user) }
+  let(:consumer) { FactoryGirl.create(:consumer) }
+  let(:display_name) { Faker::Name.name }
+  let(:mail) { Faker::Internet.email }
+  let(:response_type) { 'token' }
+  let(:state) { Faker::Lorem.characters(20) }
+
+  def generate_authenticate_session
+    session[:client_id] = consumer.uuid
+    session[:state] = state
+  end
+
+  def handle_shibboleth_expectation(expected_user)
+    expect(session[:uid]).to eq(expected_user.uid)
+    expect(session[:display_name]).to eq(display_name)
+    expect(session[:mail]).to eq(mail)
+  end
+
+  def login_shib_user(user, display_name, mail)
+    @request.env['omniauth.auth'] = {
+      uid: user.uid,
+      info: {
+        name: display_name,
+        mail: mail
+      }
+    }
+  end
+
+  def generate_shib_session(user)
+    session[:uid] = user.uid
+    session[:display_name] = display_name
+    session[:mail] = mail
+  end
+
+  def authorized_consumer_expectation
+    expect(assigns(:token)).to be
+    token = assigns(:token)
+    access_token = consumer.signed_token({
+      access_token: token
+    })
+    token_ttl = $redis.ttl(token)
+    params = {
+      access_token: access_token,
+      token_type: 'Bearer',
+      state: session[:state],
+      expires_in: token_ttl,
+      scope: Rails.application.config.default_scope
+    }
+    expect(response).to redirect_to(consumer.redirect_uri+'#'+params.to_query)
+  end
+
+  def unexpected_request_response
+    expect(response.status).to eq(401)
+    expect(response.body).to eq('invalid_request')
+  end
+
+  describe 'authenticate' do
+    it 'should respond with 401 and invalid_request if no url parameters are present' do
+      get :authenticate
+      unexpected_request_response
+    end
+
+    it 'should respond with 401 and invalid_request if consumer does not exist for the client_id specified' do
+      non_existent_client_id = SecureRandom.uuid
+      expect(non_existent_client_id).not_to eq(consumer.uuid)
+      expect(Consumer.where(uuid: non_existent_client_id)).not_to exist
+      get :authenticate,
+        client_id: non_existent_client_id,
+        response_type: response_type,
+        scope: Rails.application.config.default_scope,
+        state: state
+      unexpected_request_response
+    end
+
+    it 'should respond with 401 and invalid_request if state is not present' do
+      get :authenticate,
+        client_id: consumer.uuid,
+        response_type: response_type,
+        scope: Rails.application.config.default_scope
+      unexpected_request_response
+    end
+
+    it 'should create session and redirect_to shibboleth_login_url if consumer exists and required parameters are present' do
+      get :authenticate,
+        client_id: consumer.uuid,
+        response_type: response_type,
+        scope: Rails.application.config.default_scope,
+        state: state
+      expect(session[:client_id]).to eq(consumer.uuid)
+      expect(session[:state]).to eq(state)
+      # scope and response_type are there, but currently unused
+      expect(response).to redirect_to(shibboleth_login_url)
+    end
+  end
+
+  describe 'handle_shibboleth' do
+   describe 'when user has not already authorized the consumer' do
+     it 'should set user session and redirect_to authorize' do
+       generate_authenticate_session
+       login_shib_user(first_time_user, display_name, mail)
+       get :handle_shibboleth
+       handle_shibboleth_expectation(first_time_user)
+       expect(response).to redirect_to(authorize_url)
+     end
+   end
+
+   describe 'when user has already authorized the consumer' do
+     it 'should create access_token and redirect_to consumer.redirect_uri with expected fragment url parameters' do
+       generate_authenticate_session
+       login_shib_user(user, display_name, mail)
+       get :handle_shibboleth
+       handle_shibboleth_expectation(user)
+       authorized_consumer_expectation
+     end
+   end
+  end
+
+  describe 'authorize' do
+    it 'should present the user with a form to allow the consumer access to their profile' do
+      generate_authenticate_session
+      generate_shib_session(first_time_user)
+      get :authorize
+      expect(assigns(:user)).to be
+      expect(assigns(:user).uid).to eq(first_time_user.uid)
+      expect(assigns(:user)).not_to be_persisted
+    end
+  end
+
+  describe 'process_authorization' do
+   describe 'when user declines to authorize the consumer to access their profile' do
+     it 'should not create the new user, and redirect_to consumer.uri with expected fragment url parameters' do
+       generate_authenticate_session
+       generate_shib_session(first_time_user)
+       expect {
+         post :process_authorization, submitted: 'declined'
+       }.to_not change{User.count}
+       expect(User.where(uid: first_time_user.uid)).not_to exist
+       params = {
+         error: 'access_denied',
+         state: session[:state]
+       }
+       expect(response).to redirect_to(consumer.redirect_uri+'#'+params.to_query)
+     end
+   end
+
+   describe 'when user authorizes the consumer to access their profile' do
+     it 'should create new user and access_token and redirect_to consumer.redirect_uri with expected fragment url parameters' do
+       generate_authenticate_session
+       generate_shib_session(first_time_user)
+       expect {
+         post :process_authorization, submitted: 'allow'
+       }.to change{User.count}.by(1)
+       expect(User.where(uid: first_time_user.uid)).to exist
+       authorized_consumer_expectation
+     end
+   end
+  end
+end
