ed1505c59fc2e64216d2ae995ea3775e3769bc4b
Darin London <darin.london@duke.edu>
2015-06-16 09:31:17 -0400

    user.token requires a client_id (which should be a Consumer uuid) spec tests that a client_id is required, and passes the client_id in all tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,17 @@ require 'securerandom'
 class User < ActiveRecord::Base
   validates :uid, presence: true, uniqueness: true
 
-  def token(**params)
-    params.symbolize_keys!
-    [:mail, :display_name, :scope].each do |rkey|
-      unless params.has_key? rkey
+  def token(**credentials)
+    credentials.symbolize_keys!
+    [:client_id, :mail, :display_name, :scope].each do |rkey|
+      unless credentials.has_key? rkey
         raise ArgumentError, "#{rkey} required"
       end
     end
     new_token = SecureRandom.hex
-    credentials = {'uid': uid, 'mail': params[:mail], 'display_name': params[:display_name], 'scope': params[:scope]}.to_json
+    credentials[:uid] = uid
     $redis.multi do
-      $redis.set new_token, credentials
+      $redis.set new_token, credentials.to_json
       $redis.expire new_token, Rails.application.config.token_ttl
     end
     new_token
