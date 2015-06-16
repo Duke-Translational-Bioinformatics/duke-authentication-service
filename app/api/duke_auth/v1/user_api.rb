b85e76b92b9c482d06864f2d0de9727eb59cd363
Darrin Mann <darrin.mann@duke.edu>
2015-06-16 11:05:51 -0400

    Setting up the token_info endpoint and specs. Not fully fleshed out yet.

--- app/api/duke_auth/v1/user_api.rb
+++ app/api/duke_auth/v1/user_api.rb
@@ -1,13 +1,16 @@
 module DukeAuth
   module V1
     class UserAPI < Grape::API
-      desc 'profile' do
-        detail 'This allows a consumer to get a user profile'
-        named 'profile'
-        failure [404]
+      desc 'token_info' do
+        detail 'This allows a consumer to validate a token and get a user profile'
+        named 'token_info'
+        failure [400]
       end
-      get '/profile/:id', root: false do
-        
+      params do
+        optional :access_token, type: String
+      end
+      get '/token_info', root: false do
+        error!({error: 'invalid_token'},400)
       end
     end
   end
