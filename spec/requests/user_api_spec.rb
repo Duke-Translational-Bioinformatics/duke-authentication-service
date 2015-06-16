e950c691425def2c7f71dadb8c12952c37f2b915
Darin London <darin.london@duke.edu>
2015-06-16 11:08:31 -0400

    Merge branch 'develop' of github.com:Duke-Translational-Bioinformatics/duke-authentication-service into develop

--- spec/requests/user_api_spec.rb
+++ spec/requests/user_api_spec.rb
@@ -2,72 +2,58 @@ require 'rails_helper'
 require 'jwt'
 
 describe DukeAuth::V1::UserAPI do
-  def missing_token_expectations
-    expect(response.status).to eq(401)
-    response_json = JSON.parse(response.body)
-    expect(response_json).to have_key('error')
-    expect(response_json['error']).to eq(401)
-    expect(response_json).to have_key('reason')
-    expect(response_json['reason']).to eq('consumer not recognized')
-    expect(response_json).to have_key('suggestion')
-    expect(response_json['suggestion']).to eq('only recognized consumers can use this service')
-  end
-
-  def invalid_token_expectations
-    expect(response.status).to eq(401)
-    response_json = JSON.parse(response.body)
-    expect(response_json).to have_key('error')
-    expect(response_json['error']).to eq(401)
-    expect(response_json).to have_key('reason')
-    expect(response_json['reason']).to eq('consumer not recognized')
-    expect(response_json).to have_key('suggestion')
-    expect(response_json['suggestion']).to eq('only recognized consumers can use this service')
-  end
-
-  def wrong_id_expectations
-    expect(response.status).to eq(403)
-    response_json = JSON.parse(response.body)
-    expect(response_json).to have_key('error')
-    expect(response_json['error']).to eq(403)
-    expect(response_json).to have_key('reason')
-    expect(response_json['reason']).to eq('forbidden')
-    expect(response_json).to have_key('suggestion')
-    expect(response_json['suggestion']).to eq('you do not have the ability to perform this operation')
-  end
-
   let(:json_headers) { { 'Accept' => 'application/json', 'Content-Type' => 'application/json'} }
-  let(:consumer) {$consumer = FactoryGirl.create(:consumer)}
+  let(:consumer) {FactoryGirl.create(:consumer)}
+  let (:user) { FactoryGirl.create(:user) }
+  let(:display_name) { Faker::Name.name }
+  let(:mail) { Faker::Internet.email }
+  let(:scope) { 'display_name mail uid' }
+  let (:token) { 
+    user.token(
+      client_id: consumer.uuid,
+      display_name: display_name, 
+      mail: mail, 
+      scope: scope
+    ) 
+  }
 
-  describe 'get profile' do
-    it 'should return user profile with a registered consumer jwt once after successful login' do
-      get "/api/v1/profile/#{user.id}", nil, {'Authorization' => user_token}.merge(json_headers)
+  describe 'get token_info' do
+    it 'should respond with a JSON object that describes the valid token' do
+      get "/api/v1/token_info/", {access_token: 'valid_token'}, json_headers
       expect(response.status).to eq(200)
-    end
+      expect(response.body).to be
+      expect(response.body).not_to eq('null')
 
-    it 'should not return their profile more than once after a single login' do
-      get "/api/v1/profile/#{user.id}", nil, {'Authorization' => user_token}.merge(json_headers)
-      expect(response.status).to eq(404)
       response_json = JSON.parse(response.body)
-      expect(response_json).to have_key('error')
-      expect(response_json['error']).to eq(404)
-      expect(response_json).to have_key('reason')
-      expect(response_json['reason']).to eq('user not found')
-      expect(response_json).to have_key('suggestion')
-      expect(response_json['suggestion']).to eq('this user does not exist, or has not logged in')
+      expect(response_json).to have_key('audience')
+      expect(response_json['audience']).to eq('FOO')
+      expect(response_json).to have_key('uid')
+      expect(response_json['uid']).to eq('FOO')
+      expect(response_json).to have_key('scope')
+      expect(response_json['scope']).to eq('FOO')
+      expect(response_json).to have_key('expires_in')
+      expect(response_json['expires_in']).to eq(999)
     end
 
-    it 'should return an appropriate error if the jwt is not present' do
-      get "/api/v1/user/#{user.id}", json_headers
-      missing_token_expectations
+    it 'should respond with an error for an expired token' do
+      get "/api/v1/token_info/", {access_token: 'expired_token'}, json_headers
+      expect(response.status).to eq(400)
+      expect(response.body).to be
+      expect(response.body).to eq('{"error":"invalid_token"}')
     end
 
-    it 'should return an appropriate error if the jwt is invalid' do
-      get "/api/v1/user/#{user.id}", nil, {'Authorization': 'invalidToken'}.merge(json_headers)
-      invalid_token_expectations
+    it 'should respond with an error for a tampered token' do
+      get "/api/v1/token_info/", {access_token: 'tampered_token'}, json_headers
+      expect(response.status).to eq(400)
+      expect(response.body).to be
+      expect(response.body).to eq('{"error":"invalid_token"}')
     end
-    it 'should return an appropriate error if the user is withdrawn' do
-      get "/api/v1/user/#{user.id}", nil, {'Authorization': withdrawn_user.auth_token}.merge(json_headers)
-      withdrawn_user_token_expectations(withdrawn_user)
+
+    it 'should respond with an error for a revoked token' do
+      get "/api/v1/token_info/", {access_token: 'revoked_token'}, json_headers
+      expect(response.status).to eq(400)
+      expect(response.body).to be
+      expect(response.body).to eq('{"error":"invalid_token"}')
     end
-  end #get profile
+  end #get token_info
 end
