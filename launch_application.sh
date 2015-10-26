b5dd25cd67f2fa69b08c4c984330db025b871fcf
Darin London <darin.london@duke.edu>
2015-10-26 10:22:46 -0400

    dc-dev.utils.yml: consumer had 2 env_file entries, which caused problems. Consolidated them. launch_application.sh needs to sleep 10 after the db server starts before running rake db:migrate, etc.   because Oracle is slow to start!

--- launch_application.sh
+++ launch_application.sh
@@ -3,10 +3,12 @@
 if [ -z $COMPOSE_FILE ]
 then
   docker-compose up -d
+  sleep 10
   docker-compose -f dc-dev.utils.yml run rake db:migrate
   docker-compose -f dc-dev.utils.yml run consumer
 else
   docker-compose up -d rproxy
+  sleep 10
   docker-compose run rake db:migrate
   docker-compose run consumer
 fi
